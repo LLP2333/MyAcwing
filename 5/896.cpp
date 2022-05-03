b175a7b0ffa454a2fd667734f6822fc67a4fe6c6
llp <llp2333@qq.com>
2022-05-03 14:49:13 +0800

    最长公共之序列

--- 5/896.cpp
+++ 5/896.cpp
@@ -5,8 +5,7 @@
 using namespace std;
 const int N=1e5+10;
 int n;
-int a[N],f[N];//f[i]表示以i为最后节点的最长上升子序列
-int ans;
+int a[N],q[N];//q[i]表示长度为i的的序列的结尾最小元素
 
 int main()
 {
@@ -14,14 +13,32 @@ int main()
     for(int i=1;i<=n;i++)
     {
         scanf("%d",&a[i]);
-        for(in)
 
     }
+    int len=0;
+   // q[0]=-2e9;//q[0]小于所有数，所以无论后面的数是啥都能插在q[0]的后面（看别人说的），但是我发现好像没什么用，mid不可能等于0,
+    for(int i=1;i<=n;i++)  //i从1到n
+    {
+        int l=0,r=len;
+        while (l<r)
+        {
+            int mid=l+r+1>>1;
+            if(q[mid]<a[i]) l=mid;//找右边
+            else r=mid-1;//找左边
+            
+        }//l==r循环退出，找到比a[i]小q[l]或者q[r]
+        len=max(len,r+1);//果a[i]比q[len]大的话,那么最大的最长上升子序列的值应当加1;
+        q[r+1]=a[i];//更新一下  q[r+1]>=a[i]
+        
+    }
+
+
+    printf("%d\n",len);
    
 
  
 
-    cout<<ans<<endl;
+
     return 0;
     
 }
