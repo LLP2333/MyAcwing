b5436a7107e2a91c77fae6317f2d0a8101d82364
llp <llp2333@qq.com>
2022-05-25 15:14:03 +0800

    NASK

--- 5/900.cpp
+++ 5/900.cpp
@@ -1,5 +1,8 @@
+//状态转移方程  f[i][j]=f[i-1][j]+f[i-1][j-i],可以优化成一维，把 i优化掉
+//表示0-i个物品，j表示背包总容量，f[i][j]属性为数量,f[0][0]=1;
 #include<iostream>
 #include<cstring>
 #include<algorithm>
 #include<cstdio>
 using namespace std;
+
